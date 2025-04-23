a4e13fda1946ada9696d080535f386cfb7939b69
DRRLW <756859190@qq.com>
2025-04-23 13:02:54 +0100

    first tests

--- FindBluetooth.py
+++ FindBluetooth.py
@@ -2,9 +2,9 @@ import asyncio
 from bleak import BleakScanner
 
 async def main():
-    print("🔍 正在扫描附近的 BLE 设备...")
+    print("Scanning for BLE devices..")
     devices = await BleakScanner.discover(timeout=10.0)
     for i, d in enumerate(devices):
-        print(f"{i+1}. 名称: {d.name}, 地址: {d.address}")
+        print(f"{i+1}. Name: {d.name}, MAC Address: {d.address}")
 
 asyncio.run(main())
