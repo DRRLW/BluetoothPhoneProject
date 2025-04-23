cd51a140a40cf28b9c69b1b0d55395c7ff455c66
DRRLW <756859190@qq.com>
2025-04-23 13:05:46 +0100

    Modify the language

--- FindBluetooth.py
+++ FindBluetooth.py
@@ -2,7 +2,7 @@ import asyncio
 from bleak import BleakScanner
 
 async def main():
-    print("Scanning for BLE devices..")
+    print("Scanning for BLE devices...")
     devices = await BleakScanner.discover(timeout=10.0)
     for i, d in enumerate(devices):
         print(f"{i+1}. Name: {d.name}, MAC Address: {d.address}")
